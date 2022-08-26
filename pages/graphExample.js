938643aa26b8e77d030330b240e068bb9eb03393
2ghost <jdes994506@gmail.com>
2022-08-26 23:11:56 +0800

    first commit

---
+++ pages/graphExample.js
@@ -0,0 +1,22 @@
+import { useQuery, gql } from "@apollo/client";
+
+const GET_ACTIVE_ITEMS = gql`
+  {
+    activeItems(
+      first: 5
+      where: { buyer: "0x0000000000000000000000000000000000000000" }
+    ) {
+      id
+      buyer
+      nftAddress
+      seller
+      price
+    }
+  }
+`;
+
+export default function graphExample() {
+  const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS);
+  console.log(data);
+  return <div>hi</div>;
+}
